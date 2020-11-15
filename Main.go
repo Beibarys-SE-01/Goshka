ce5e6c0c98131e6f3f517875d6db617526f91747
yolo62442 <yolo62442@gmail.com>
2020-11-15 22:48:07 +0600

    Winter is coming...

--- Main.go
+++ Main.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	/*facade := newGameFacade()
+	facade := newGameFacade()
 	facade.createCharacter()
-	facade.startGame(facade.createLevels())*/
-	db := GetSingletonDatabase()
-	number := db.GetNumber("Kuka")
-	fmt.Println("Phone number of Kuka is", number)
+	facade.startGame(facade.createLevels())
 }
 
 
